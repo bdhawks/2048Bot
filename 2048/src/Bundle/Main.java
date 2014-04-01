f9a830c00ab9715e969aa0e5e77c1ec6fc5f6704
bdhawks <bdhawks@gmail.com>
2014-04-01 15:08:28 -0500

    Update Main.java

--- 2048/src/Bundle/Main.java
+++ 2048/src/Bundle/Main.java
@@ -2,13 +2,19 @@ package Bundle;
 
 import java.awt.AWTException;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Rectangle;
 import java.awt.Robot;
+import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javax.imageio.ImageIO;
 
 /**
  *
@@ -31,24 +37,64 @@ public class Main
     public static void main(String[] args) throws InterruptedException 
     {
         Thread.sleep(2000);
+        
+        try {
+			updateGrid();
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.toString());
+		}
+		
         pause();
     }
     static void updateGrid() throws AWTException
     {
-        ArrayList<Color> colors=new ArrayList();
-        ArrayList<String> tiles=new ArrayList();
+        ArrayList<Color> colors=new ArrayList<Color>();
+        ArrayList<String> tiles=new ArrayList<String>();
         int[][][] grid=new int[4][4][1];
         Robot r=new Robot();
-        Rectangle rect=new Rectangle(700, 310, 505, 505);
+        Rectangle rect = new Rectangle(700, 310, 505, 505);
         int y=((int)rect.getY()+40);
         int x=((int)rect.getX()+30);
+        
+        retrieveScreenResolution();
+        
+        Color c;
+        
+        try{
+        
+        BufferedImage image = r.createScreenCapture(rect);
+
+           int w = image.getWidth();
+           int h = image.getHeight();
+
+           int[] dataBuffInt = image.getRGB(0, 0, w, h, null, 0, w); 
+
+           
+           Color prevColor = new Color(0);
+           for(int i=0; i < dataBuffInt.length; i ++){
+        	   c = new Color(dataBuffInt[i]);
+        	   if(c.getRGB() != prevColor.getRGB()){
+        	   System.out.println("Next Color:");
+        	   System.out.println(c.getRed());   // = (dataBuffInt[100] >> 16) & 0xFF
+        	   System.out.println(c.getGreen()); // = (dataBuffInt[100] >> 8)  & 0xFF
+        	   System.out.println(c.getBlue());  // = (dataBuffInt[100] >> 0)  & 0xFF
+
+        	   }
+        	  
+        	  prevColor = c;
+           }
+        }catch(Exception e){
+        	System.out.println(e.toString());
+        }
+        
         for(int i=0;i<4;i++)
         {
-            for(int b=0;b<4;b++)
+            for(int b=0; b<4; b++)
             {
-                Color c=r.getPixelColor(x,y);
-                grid[i][b][0]=colors.indexOf(c);
-                if(colors.indexOf(c)>0)
+                c = r.getPixelColor(x,y);
+                grid[i][b][0] = colors.indexOf(c);
+                if(colors.indexOf(c) > 0)
                 {
                     tiles.add(i+","+b+","+colors.indexOf(c)); //add tiles that are not empty to tiles arraylist
                 }
@@ -57,9 +103,9 @@ public class Main
             x=((int)rect.getX()+30);
             y+=120;
         }
-        for(int i=0;i<4;i++)
+        for(int i=0; i<4; i++)
         {
-            for(int b=0;b<4;b++)
+            for(int b=0; b<4; b++)
             {
                 System.out.println(grid[i][b][0]+" tile @"+ "("+i+","+b+")"); //prints detected tiles and values
             }
@@ -73,8 +119,8 @@ public class Main
     {
         //use first 2 lower value pieces
         //compare x and y differences, move piece according to least difference
-        int atileX=Integer.parseInt(pieces.get(0).substring(0, 1));
-        int btileX=Integer.parseInt(pieces.get(0+1).substring(0,1));
+        int atileX = Integer.parseInt(pieces.get(0).substring(0, 1));
+        int btileX = Integer.parseInt(pieces.get(0+1).substring(0,1));
         int atileY=Integer.parseInt(pieces.get(0).substring(2,3));
         int btileY=Integer.parseInt(pieces.get(0+1).substring(2,3));
         if(Integer.compare(atileY, btileY)<0)
@@ -88,6 +134,16 @@ public class Main
             }
         }
     }
+    
+    static void retrieveScreenResolution(){
+    	
+    	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    	double width = screenSize.getWidth();
+    	double height = screenSize.getHeight();
+    	
+    	System.out.println("Screen Resolution:" + width + "," + height);
+    }
+    
     static void pause()
     {
         Scanner scan=new Scanner(System.in);
